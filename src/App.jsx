c107f66d39fd76326b22467996263f85294a6f8c
ben <benjamin.zelinsky@gmail.com>
2025-09-26 13:31:15 +0200

    Add fruits. Make buttons add and subtract, using useState.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 
 import './App.css'
+import React from 'react';
 
 function App() {
-
-  return (
-    <>
-        <h1>Fruitmand bezorgservice</h1>
-    </>
-  )
+    const [amount, setAmount] = React.useState(0);
+    return (
+        <>
+            <div>
+                <h2>🍓Aardbeien</h2>
+                <button
+                    onClick={() => setAmount(amount-1)}
+                >-</button>
+                <p>{amount}</p>
+                <button
+                    onClick={() => setAmount(amount+1)}
+                >+</button>
+            </div>
+            <h2>🍌Bananen</h2>
+            <h2>🍏Appels</h2>
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
+            <h2>🥝Kiwi's</h2>
+        </>
+    )
 }
 
 export default App
