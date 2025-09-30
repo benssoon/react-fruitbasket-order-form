a3008362cf68b0e576f9cb94c935d647cf540365
ben <benjamin.zelinsky@gmail.com>
2025-09-30 08:47:56 +0200

    Make fruit counter component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 
-import './App.css'
+import './App.css';
 import React from 'react';
 import {useState} from 'react';
+import Counter from './components/Counter.jsx';
 
 function App() {
     // State setup for whole form
@@ -58,67 +59,29 @@ function App() {
         console.log(formState)
     }
 
+    function handleReset() {
+        setAmountStrawberries(0);
+        setAmountBananas(0);
+        setAmountApples(0);
+        setAmountKiwis(0);
+    }
+
     const [amountStrawberries, setAmountStrawberries] = React.useState(0);
     const [amountBananas, setAmountBananas] = React.useState(0);
     const [amountApples, setAmountApples] = React.useState(0);
     const [amountKiwis, setAmountKiwis] = React.useState(0);
+    const [amountFruit, setAmountFruit] = React.useState(0);
     return (
 
         <>
             <div className="counters">
-                <div className="counter">
-                    <h2>🍓Aardbeien</h2>
-                    <button
-                        onClick={() => setAmountStrawberries(amountStrawberries-1<0 ? 0 : amountStrawberries-1)}
-                    >-</button>
-                    <p>{amountStrawberries}</p>
-                    <button
-                        onClick={() => setAmountStrawberries(amountStrawberries + 1)}
-                    >+</button>
-                </div>
-                <div className="counter">
-                    <h2>🍌Bananen</h2>
-                    <button
-                        onClick={() => setAmountBananas(amountBananas-1<0 ? 0 : amountBananas-1)}
-                    >-</button>
-                    <p>{amountBananas}</p>
-                    <button
-                        onClick={() => setAmountBananas(amountBananas + 1)}
-                    >+</button>
-                </div>
-                <div className="counter">
-                    <h2>🍏Appels</h2>
-                    <button
-                        onClick={() => setAmountApples(amountApples-1<0 ? 0 : amountApples-1)}
-                    >-
-                    </button>
-                    <p>{amountApples}</p>
-                    <button
-                        onClick={() => setAmountApples(amountApples + 1)}
-                    >+
-                    </button>
-                </div>
-
-                <div className="counter">
-                    {/* eslint-disable-next-line react/no-unescaped-entities */}
-                    <h2>🥝Kiwi's</h2>
-                    <button
-                        onClick={() => setAmountKiwis(amountKiwis-1<0 ? 0 : amountKiwis-1)}
-                    >-
-                    </button>
-                    <p>{amountKiwis}</p>
-                    <button
-                        onClick={() => setAmountKiwis(amountKiwis + 1)}
-                    >+
-                    </button>
-                </div>
+                <Counter amountFruit={amountFruit} setAmountFruit={setAmountFruit} title="🍓Aardbeien" />
+                <Counter amountFruit={amountFruit} setAmountFruit={setAmountFruit} title="🍌Bananen" />
+                <Counter amountFruit={amountFruit} setAmountFruit={setAmountFruit} title="🍏Appels" />
+                <Counter amountFruit={amountFruit} setAmountFruit={setAmountFruit} title="🥝Kiwi's" />
+                
                 <button
-                    onClick={() => {
-                        setAmountStrawberries(0);
-                        setAmountBananas(0);
-                        setAmountApples(0);
-                        setAmountKiwis(0);
-                    }}
+                    onClick={handleReset}
                 >Reset</button>
             </div>
 
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,20 +1,19 @@
-function Counter() {
+import './Counter.css';
+function Counter({amountFruit, setAmountFruit, title}) {
     return (
-        <>
-            <h2>🍓Aardbeien</h2>
-            <button
-                onClick={() => {
-                    setAmount(amount-1<0 ? 0 : amount-1);
-                    console.log(amount);
-                }}
-            >-</button>
-            <p>{amount}</p>
-            <button
-                onClick={() => {
-                    setAmount(amount + 1);
-                    console.log(amount);
-                }}
-            >+</button>
-        </>
+
+            <div className="counter">
+                <h2>{title}</h2>
+                <button
+                    onClick={() => setAmountFruit(amountFruit-1<0 ? 0 : amountFruit-1)}
+                >-</button>
+                <p>{amountFruit}</p>
+                <button
+                    onClick={() => setAmountFruit(amountFruit + 1)}
+                >+</button>
+            </div>
+
     )
 }
+
+export default Counter;
